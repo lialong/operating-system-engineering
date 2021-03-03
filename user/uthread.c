3403b74b25447eaf0d076f7c83fdd2e050089e2e
zelong.li <437516156@qq.com>
2021-03-03 23:38:43 +0800

    类型强转

--- user/uthread.c
+++ user/uthread.c
@@ -81,7 +81,7 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
-  t->stack = (char [])malloc(STACK_SIZE);
+  t->stack = (char*)malloc(STACK_SIZE);
 	t->threadContext.sp = (uint64)(t->stack);
 }
 
