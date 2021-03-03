5e42023aa9cecceb385f30c815169900fb80bb22
zelong.li <437516156@qq.com>
2021-03-03 23:26:40 +0800

    头文件引用

--- user/uthread.c
+++ user/uthread.c
@@ -1,6 +1,7 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
+#include "kernel/riscv.h"
 #include "kernel/spinlock.h"
 #include "kernel/param.h"
 #include "kernel/proc.h"
