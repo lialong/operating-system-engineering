0d45656969a7fa2a6267628ccb0b19f6f881609b
zelong.li <437516156@qq.com>
2021-03-03 23:54:09 +0800

    恢复线程context时，ra要赋值

--- user/uthread.c
+++ user/uthread.c
@@ -81,7 +81,7 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
-	t->threadContext.ra = func;
+	t->threadContext.ra = (uint64)func;
 	t->threadContext.sp = (uint64)(t->stack);
 }
 
