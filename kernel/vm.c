d38499b80b5e9c934df61397988d4f7e50fc553d
zelong.li <437516156@qq.com>
2021-04-21 17:43:05 +0800

    fork_test

--- kernel/vm.c
+++ kernel/vm.c
@@ -306,7 +306,7 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
     if((pte = walk(old, i, 0)) == 0)
       panic("uvmcopy: pte should exist");
     if((*pte & PTE_V) == 0)
-      panic("uvmcopy: page not present");
+      continue;
     pa = PTE2PA(*pte);
     flags = PTE_FLAGS(*pte);
     if((mem = kalloc()) == 0)
